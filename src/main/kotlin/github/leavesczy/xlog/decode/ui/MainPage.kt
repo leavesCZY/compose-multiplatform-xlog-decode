dbb96dc075c8f8e4567aaf42ed0bb8c8aa5c80c0
leavesCZY <1990724437@qq.com>
2025-05-07 12:14:39 +0800

    optimize code

--- src/main/kotlin/github/leavesczy/xlog/decode/ui/MainPage.kt
+++ src/main/kotlin/github/leavesczy/xlog/decode/ui/MainPage.kt
@@ -11,19 +11,11 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
 import androidx.compose.ui.draganddrop.DragAndDropEvent
 import androidx.compose.ui.draganddrop.DragAndDropTarget
 import androidx.compose.ui.draganddrop.DragData
 import androidx.compose.ui.draganddrop.dragData
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawWithCache
-import androidx.compose.ui.geometry.CornerRadius
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.PathEffect
-import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.FrameWindowScope
@@ -258,33 +250,3 @@ private fun RuntimeLog(
         )
     }
 }
-
-private fun Modifier.dashedBorder(
-    strokeWidth: Dp,
-    color: Color,
-    radius: Dp
-) = composed(
-    factory = {
-        val density = LocalDensity.current
-        val strokeWidthPx = with(density) {
-            strokeWidth.toPx()
-        }
-        val cornerRadius = with(density) {
-            radius.toPx()
-        }
-        then(
-            other = Modifier.drawWithCache {
-                onDrawBehind {
-                    drawRoundRect(
-                        color = color,
-                        style = Stroke(
-                            width = strokeWidthPx,
-                            pathEffect = PathEffect.dashPathEffect(intervals = floatArrayOf(12f, 12f), phase = 6f)
-                        ),
-                        cornerRadius = CornerRadius(x = cornerRadius, y = cornerRadius)
-                    )
-                }
-            }
-        )
-    }
-)
